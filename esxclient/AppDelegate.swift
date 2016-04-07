f691088a24295f0ee0225473c9f06a9a44a9df8b
Scott J. Goldman <scottjg@github.com>
2016-04-07 13:13:24 -0700

    Initial Commit

---
+++ esxclient/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  esxclient
+//
+//  Created by Scott Goldman on 4/7/16.
+//  Copyright © 2016 scottjg. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
